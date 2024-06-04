7a3a90b6da50e00659ad9cf3e5425ad89d284177
as <git@schimpf.link>
2024-06-04 12:44:47 +0200

    cont

--- eqwalizer/pass_unspecced/unspecced_1.erl
+++
@@ -1,8 +0,0 @@
--module(unspecced_1).
-%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
-%%% This source code is licensed under the Apache 2.0 license found in 
-%%% the LICENSE file in the root directory of this source tree.
--compile([export_all, nowarn_export_all]).
--record(rec, {id :: number()}).
--spec expect_none(none()) -> none().
-expect_none(None) -> None.
